c8e53b848310f109ffb5c01b88030948daead188
rachel-soon <rachel_may0106@hotmail.com>
2025-01-19 23:18:49 +0800

    feat: override default radix font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import "./App.css";
+import {
+  Box,
+  Flex,
+  Card,
+  Grid,
+  TextArea,
+  Text,
+  Switch,
+  Button,
+} from "@radix-ui/themes";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box maxWidth="400px">
+        <Card size="2">
+          <Flex direction="column" gap="3">
+            <Grid gap="1">
+              <Text as="div" weight="bold" size="2" mb="1">
+                Feedback
+              </Text>
+              <TextArea placeholder="Write your feedback…" />
+            </Grid>
+            <Flex asChild justify="between">
+              <label>
+                <Text color="gray" size="2">
+                  Attach screenshot?
+                </Text>
+                <Switch size="1" defaultChecked />
+              </label>
+            </Flex>
+            <Grid columns="2" gap="2">
+              <Button variant="surface">Back</Button>
+              <Button>Send</Button>
+            </Grid>
+          </Flex>
+        </Card>
+      </Box>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import "@radix-ui/themes/styles.css";
+import "./index.css";
+
 import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Theme>
+    <Theme appearance="dark" accentColor="orange">
       <App />
     </Theme>
   </StrictMode>
