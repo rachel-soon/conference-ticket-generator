d8781777a258688e82b0a14ef9446f792fea3d84
rachel-soon <rachel_may0106@hotmail.com>
2025-01-20 21:53:16 +0800

    build: uninstall react-hook-form and install radix ui form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Background from "./Background/Background";
 import Logo from "./Logo/Logo";
+import TicketForm from "./TicketForm/TicketForm";
 
 function App() {
   return (
     <Background>
       <Logo />
+
+      {/* Form */}
+
+      <TicketForm />
     </Background>
   );
 }
--- src/TicketForm/TicketForm.tsx
+++ src/TicketForm/TicketForm.tsx
@@ -1,3 +1,39 @@
+import { Box, TextField } from "@radix-ui/themes";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
 export default function TicketForm() {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  //   console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{ display: "flex", flexDirection: "column", zIndex: "10" }}
+      >
+        <Box maxWidth="500px">
+          <TextField.Root size="3" placeholder="Search the docs…" />
+        </Box>
+        {/* <input defaultValue="test" {...register("example")} /> */}
+
+        <input {...register("exampleRequired", { required: true })} />
+
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
