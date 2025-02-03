e899cdb970c1f596cd106b84fcd9f611c09bd646
rachel-soon <rachel_may0106@hotmail.com>
2025-02-03 22:54:41 +0800

    feat: add image preview and buttons

--- src/TicketForm/Avatar/index.tsx
+++ src/TicketForm/Avatar/index.tsx
@@ -8,26 +8,26 @@ import React, { useState } from "react";
 export default function TicketFormAvatar() {
   const [fileSizeExceeded, setFileSizeExceeded] = useState(false);
   const [fileTypeError, setFileTypeError] = useState(false);
-  const formData = new FormData();
+  const [formData, setFormData] = useState<File>();
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const file = event.target.files[0];
-      formData.append("file", file);
 
       // get the size in kb
       // 1 byte = 1024kb
       const fileSize = Math.floor(event.target.files[0].size / 1024);
-      if (fileSize > 500) {
-        setFileSizeExceeded(true);
-      } else setFileSizeExceeded(false);
-
-      // validate file type
       const fileType = file.type.split("/")[1];
 
-      if (!["jpg", "jpeg", "png"].includes(fileType)) {
+      if (fileSize > 500) {
+        setFileSizeExceeded(true);
+      } else if (!["jpg", "jpeg", "png"].includes(fileType)) {
         setFileTypeError(true);
-      } else setFileTypeError(false);
+      } else {
+        setFileSizeExceeded(false);
+        setFileTypeError(false);
+        setFormData(file);
+      }
     }
   };
   return (
@@ -47,14 +47,45 @@ export default function TicketFormAvatar() {
           />
         </Form.Control>
 
-        <div className="uploadIconContainer avatar__icon-container">
-          <img src={iconUpload} alt="upload icon" className="avatar__icon" />
-          <p className="uploadText avatar__icon-text">
-            Drag and drop or click to upload
-          </p>
-        </div>
+        {/* Image preview */}
+        {formData ? (
+          <div className="avatar__preview">
+            <div
+              style={{
+                marginBottom: "6px",
+                textAlign: "center",
+              }}
+            >
+              <img
+                src="https://media.istockphoto.com/id/182174009/photo/curious-bunny.jpg?s=1024x1024&w=is&k=20&c=-MZ2Ik6UH0RRmp7Gd4P4rdryVt85juR6WWWAmQ7OhZI="
+                style={{
+                  objectFit: "cover",
+                  height: "70px",
+                  width: "70px",
+                  borderRadius: "15px",
+                  border: "1px solid var(--neutral-0)",
+                }}
+              />
+            </div>
 
-        {/* <div>Image preview </div> */}
+            {/* Action buttons */}
+            <div style={{ display: "flex", gap: "15px" }}>
+              <button className="btn btn--remove" type="button">
+                Remove image
+              </button>
+              <button className="btn btn--change" type="button">
+                Change image
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="avatar__icon-container">
+            <img src={iconUpload} alt="upload icon" className="avatar__icon" />
+            <p className="avatar__icon-text">
+              Drag and drop or click to upload
+            </p>
+          </div>
+        )}
       </div>
 
       <p className="avatar__msg">
