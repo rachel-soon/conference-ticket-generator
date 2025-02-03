3e2335f07c40aefa1fda5c6608d1e655606d0585
rachel-soon <rachel_may0106@hotmail.com>
2025-02-03 22:10:34 +0800

    feat: change css naming

--- src/TicketForm/Avatar/index.tsx
+++ src/TicketForm/Avatar/index.tsx
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./style.scss";
 import * as Form from "@radix-ui/react-form";
 import { Box } from "@radix-ui/themes";
 import iconUpload from "./assets/icon-upload.svg";
@@ -8,11 +8,11 @@ import React, { useState } from "react";
 export default function TicketFormAvatar() {
   const [fileSizeExceeded, setFileSizeExceeded] = useState(false);
   const [fileTypeError, setFileTypeError] = useState(false);
+  const formData = new FormData();
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const file = event.target.files[0];
-      const formData = new FormData();
       formData.append("file", file);
 
       // get the size in kb
@@ -31,58 +31,47 @@ export default function TicketFormAvatar() {
     }
   };
   return (
-    <>
-      <Form.Field name="avatar" className="avatar">
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Form.Label className="FormLabel">
-            Upload Avatar
-            <Box className="InputUpload"></Box>
-          </Form.Label>
+    <Form.Field name="avatar" className="avatar">
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Form.Label className="FormLabel">
+          Upload Avatar
+          <Box className="InputUpload avatar__input-upload"></Box>
+        </Form.Label>
 
-          <Form.Control asChild className="FormControl">
-            <input
-              type="file"
-              className="Input"
-              onChange={handleFileChange}
-              accept=".jpg, .jpeg, .png"
-            />
-          </Form.Control>
+        <Form.Control asChild className="FormControl">
+          <input
+            type="file"
+            className="Input"
+            onChange={handleFileChange}
+            accept=".jpg, .jpeg, .png"
+          />
+        </Form.Control>
 
-          <div className="uploadIcon">
-            <img src={iconUpload} alt="upload icon" />
-          </div>
-          <p className="upload">Drag and drop or click to upload</p>
-          {/* Image display preview */}
-
-          <div></div>
+        <div className="uploadIconContainer avatar__icon-container">
+          <img src={iconUpload} alt="upload icon" className="avatar__icon" />
+          <p className="uploadText avatar__icon-text">
+            Drag and drop or click to upload
+          </p>
         </div>
 
-        <p
-          style={{
-            position: "absolute",
-            bottom: "-28px",
-            display: "flex",
-            alignItems: "center",
-            gap: "5px",
-          }}
-        >
-          <IconInfo error={fileSizeExceeded || fileTypeError} />
+        {/* <div>Image preview </div> */}
+      </div>
 
-          {fileSizeExceeded ? (
-            <span style={{ color: "var(--orange-700)" }}>
-              File too large. Please upload a photo under 500KB.
-            </span>
-          ) : fileTypeError ? (
-            <span style={{ color: "var(--orange-700)" }}>
-              File is in wrong format. Please upload JPG or PNG.
-            </span>
-          ) : (
-            <span style={{ fontSize: "14px" }}>
-              Upload your photo (JPG or PNG, max size: 500KB).
-            </span>
-          )}
-        </p>
-      </Form.Field>
-    </>
+      <p className="avatar__msg">
+        <IconInfo error={fileSizeExceeded || fileTypeError} />
+
+        {fileSizeExceeded ? (
+          <span className="avatar__msg--error">
+            File too large. Please upload a photo under 500KB.
+          </span>
+        ) : fileTypeError ? (
+          <span className="avatar__msg--error">
+            File is in wrong format. Please upload JPG or PNG.
+          </span>
+        ) : (
+          <span>Upload your photo (JPG or PNG, max size: 500KB).</span>
+        )}
+      </p>
+    </Form.Field>
   );
 }
